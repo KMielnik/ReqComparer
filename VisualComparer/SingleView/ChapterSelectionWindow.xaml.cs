a7dc19723b03f7fc2a3cab12ecdd1ceefcd33bc0
Kamil Mielnik <kamil.mielnik.km@gmail.com>
2019-08-20 09:36:27 +0200

    Rearranged folders.

---
+++ VisualComparer/SingleView/ChapterSelectionWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace VisualComparer
+{
+    /// <summary>
+    /// Interaction logic for ChapterSelectionWindow.xaml
+    /// </summary>
+    public partial class ChapterSelectionWindow : Window
+    {
+        public ChapterSelectionWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
